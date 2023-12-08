f66a752fb1718ad6e63b083abbaf839095da1f11
kiyota <kiyota230923@eggnet.ac.jp>
2023-12-08 11:54:18 +0900

    iro

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -8,9 +8,8 @@
   </head>
   <body>
   $theme-colors: (
-    "primary":    $primary,
+     <h1>{{ $title }}</h1> :    $primary,
   );
-    <h1>{{ $title }}</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
 </html>
