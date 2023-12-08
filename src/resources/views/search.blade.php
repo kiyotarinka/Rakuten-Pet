581dc04bd1f1d37ee817f44d2a7bd08a677750cc
kiyota <kiyota230923@eggnet.ac.jp>
2023-12-08 12:29:52 +0900

    di

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -1,33 +1,42 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  </head>
-  <body>
-  <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
-  
-  <h1>{{ $title }}</h1>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container">
+    <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+
+      <h1>{{ $title }}</h1>
+    </div>
   </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-  </body>
+  <h3>{{$small_text}}</h1>
+    <h4>{{$pet['title']}}</h4>
+    <select class="form-select" name="nnnnn" id="nnnnn">
+      <option selected disabled>選択してください</option>
+      @foreach ($pet['list'] as $key => $value)
+      <option value="{{ $key }}">{{ $value }}</option>
+      @endforeach
+    </select>
+    <h4>{{$genre['title1']}}</h4>
+    <select class="form-select" name="nnnnn" id="nnnnn">
+      <option selected disabled>選択してください</option>
+      @foreach ($genre['list1'] as $key => $value)
+      <option value="{{ $key }}">{{ $value }}</option>
+      @endforeach
+      <h4>{{$many}}</h1>
+        <h1>{{$search_text}}</h1>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+          crossorigin="anonymous"></script>
+</body>
+
 </html>
-<h3>{{$small_text}}</h1>
-<h4>{{$pet['title']}}</h4>
-<select class="form-select" name="nnnnn" id="nnnnn">
-  <option selected disabled>選択してください</option>
-  @foreach ($pet['list'] as $key => $value)
-    <option value="{{ $key }}">{{ $value }}</option>
-  @endforeach
-</select>
-<h4>{{$genre['title1']}}</h4>
-<select class="form-select" name="nnnnn" id="nnnnn">
-  <option selected disabled>選択してください</option>
-  @foreach ($genre['list1'] as $key => $value)
-    <option value="{{ $key }}">{{ $value }}</option>
-  @endforeach
-<h4>{{$many}}</h1>
-<h1>{{$search_text}}</h1>
