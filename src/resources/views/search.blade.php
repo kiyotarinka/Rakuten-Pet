a56602e39b312bacd2e5ffcfe2a8647ebec12c44
fukuda <fukuda230101@eggnet.ac.jp>
2023-12-08 11:46:08 +0900

    henkou

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -7,6 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   </head>
   <body>
+  $theme-colors: (
+    "primary":    $primary,
+  );
     <h1>{{ $title }}</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
@@ -19,6 +22,10 @@
     <option value="{{ $key }}">{{ $value }}</option>
   @endforeach
 </select>
-<h4>{{$genre}}</h1>
+<select class="form-select" name="nnnnn" id="nnnnn">
+  <option selected disabled>選択してください</option>
+  @foreach ($genre['list'] as $key => $value)
+    <option value="{{ $key }}">{{ $value }}</option>
+  @endforeach
 <h4>{{$many}}</h1>
 <h1>{{$search_text}}</h1>
