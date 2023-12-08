80748bd68e3fa76403d455e6df6bc2a64880f18b
fukuda <fukuda230101@eggnet.ac.jp>
2023-12-08 12:24:05 +0900

    gi

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -7,9 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   </head>
   <body>
-  <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"p{
-    width:50%
-  }>
+  <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
   
   <h1>{{ $title }}</h1>
   </div>
