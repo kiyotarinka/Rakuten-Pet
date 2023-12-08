d48c11eda432fa0a42da6d49b1e8b17445ef16b4
fukuda <fukuda230101@eggnet.ac.jp>
2023-12-08 12:21:15 +0900

    gi

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -7,10 +7,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   </head>
   <body>
-  <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
-  p{
+  <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"p{
     width:50%
-  }
+  }>
+  
   <h1>{{ $title }}</h1>
   </div>
 
