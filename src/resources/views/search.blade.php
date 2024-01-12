843f935147426455818e9a7cf43183f10b6e6995
fukuda <fukuda230101@eggnet.ac.jp>
2024-01-12 10:47:45 +0900

    iro

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -44,7 +44,7 @@
           @endforeach
         </select>
         <br><br>
-        <h1>{{$search_text}}</h1>
+        <button type="button" class="btn btn-outline-danger"><h1>{{$search_text}}</h1></button>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
