51f04226e5d018b80a1ab03c66db9acd523b8d17
fukuda <fukuda230101@eggnet.ac.jp>
2024-01-12 10:17:47 +0900

    ome-

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -43,7 +43,9 @@
           @endforeach
         </select>
         <br><br>
+        <div class="p-3 text-warning-emphasis bg-warning-subtle border border-warning-subtle rounded-3">
         <h1>{{$search_text}}</h1>
+        </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
